b6f54e97fb5274f96080ae4cf9aec15ae3cba962
Nafissa <bhjnafissa@gmail.com>
2023-01-07 15:54:26 +0100

    fix portfolio

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var i = 0;
 var j=0;
-var txt = 'Salam, I\'m Nafissa from Algiers,Algeria. I\'m a softwere engineer. Although I\'m interested in Mobile apps developement with Flutter,I find myself building simple desktop apps in my free time.'
-var txt2 ='By now you have probably assumed that I\'m a Flutter enthusiast :).' 
+var txt = 'Algiers,Algeria. '
 var speed = 8; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -17,15 +16,57 @@ function typeWriter() {
 }
 function typeWriter2() {
 
-  if (j < txt2.length) {
-    document.getElementById("demo2").innerHTML += txt2.charAt(j);
-    j++;
-    setTimeout(typeWriter2, 20);
-  }
+  // if (j < txt2.length) {
+  //   document.getElementById("demo2").innerHTML += txt2.charAt(j);
+  //   j++;
+  //   setTimeout(typeWriter2, 20);
+  // }
  
 }
-window.onload = function() {
+function functionOne(){
+ var expDiv = document.getElementById("experience");
+ console.log(expDiv);
+ if(expDiv.style.display === "none"){
+  expDiv.style.display = "block";
+ } else{
+  expDiv.style.display ="none";
+ }
+}
+function functionTwo(){
+  var expDiv = document.getElementById("experiments");
+  console.log(expDiv);
+  if(expDiv.style.display === "none"){
+   expDiv.style.display = "block";
+  } else{
+   expDiv.style.display ="none";
+  }
+ }
+ function functionThree(){
+  var expDiv = document.getElementById("education");
+  console.log(expDiv);
+  if(expDiv.style.display === "none"){
+   expDiv.style.display = "block";
+  } else{
+   expDiv.style.display ="none";
+  }
+ }
+ function functionNote(){
+  var expDiv = document.getElementById("note");
+  console.log(expDiv);
+  if(expDiv.style.display === "none"){
+   expDiv.style.display = "block";
+  } else{
+   expDiv.style.display ="none";
+  }
+ }
 
-    typeWriter();
+window.onload = function() {
+  document.getElementById("experience").style.display = "none";
+  document.getElementById("experiments").style.display = "none";
+  document.getElementById("education").style.display = "none";
+  document.getElementById("note").style.display = "none";
+  
+  // typeWriter();
+
    
   };
